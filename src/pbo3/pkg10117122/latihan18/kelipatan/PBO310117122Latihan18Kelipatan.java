db199ed50d538e10c5ee41ebfdc97c6ff0878131
BonaventuraAldino <aldysenda99@gmail.com>
2018-10-02 00:42:28 +0700

    Kelipatan

---
+++ src/pbo3/pkg10117122/latihan18/kelipatan/PBO310117122Latihan18Kelipatan.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3.pkg10117122.latihan18.kelipatan;
+
+/**
+ *
+ * @author lenovo
+ * 
+ * NAMA  : BONAVENTURA ALDINO SENDA SENI
+ * KELAS : PBO-3
+ * NIM   : 10117122
+ * Deskripsi Program : menampilkan bilangan kelipatan 3.5, mulai dari 3.5 sampai
+ *                     35.
+ * 
+ */
+public class PBO310117122Latihan18Kelipatan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        double i= 3.5;
+        while (i <= 35.0){
+            System.out.println(i);
+            i = i + 3.5;
+        }
+        System.out.println("(Developed by : Bonaventura Aldino Senda Seni)");
+    }
+}
