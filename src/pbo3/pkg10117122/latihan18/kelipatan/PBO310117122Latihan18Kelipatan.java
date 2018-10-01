0309b34e0905376af371f10da69e03684695477c
BonaventuraAldino <aldysenda99@gmail.com>
2018-10-02 06:40:09 +0700

    latihan 18

--- src/pbo3/pkg10117122/latihan18/kelipatan/PBO310117122Latihan18Kelipatan.java
+++ src/pbo3/pkg10117122/latihan18/kelipatan/PBO310117122Latihan18Kelipatan.java
@@ -9,9 +9,9 @@ package pbo3.pkg10117122.latihan18.kelipatan;
  *
  * @author lenovo
  * 
- * NAMA  : BONAVENTURA ALDINO SENDA SENI
- * KELAS : PBO-3
- * NIM   : 10117122
+ * NAMA  : AldiLesmana
+ * KELAS : If-3
+ * NIM   : 10117108
  * Deskripsi Program : menampilkan bilangan kelipatan 3.5, mulai dari 3.5 sampai
  *                     35.
  * 
@@ -28,6 +28,6 @@ public class PBO310117122Latihan18Kelipatan {
             System.out.println(i);
             i = i + 3.5;
         }
-        System.out.println("(Developed by : Bonaventura Aldino Senda Seni)");
+        System.out.println("(Developed by : Aldi Lesmana)");
     }
 }
